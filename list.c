18c705849338f243aed8396a1b3143b5154a8cbe
Replit user <replituser@example.com>
2021-03-29 14:21:40 +0000

    new try

--- list.c
+++ list.c
@@ -144,7 +144,7 @@ void * popBack(List * list) {
 }
 
 void * popCurrent(List * list) {
-  assert (list != NULL);
+  //assert (list != NULL);
 
     if (list->head == NULL || list->current == NULL) return NULL;
 
@@ -165,7 +165,7 @@ void * popCurrent(List * list) {
           aux->prev->next = aux->next;
       }
     }
-  
+
     free(aux);
    
   free(list->current);
